efb65379c3b8ae578a9e4c4189c8aaa324856f70
Ujjwal Suman <ujjwalsuman007@gmail.com>
2024-04-15 09:32:27 +0530

    Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
 
 const App = () => {
   return (
-    <h1>
-      Welcome to UrShop
-    </h1>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1> Welcome to UrShop </h1>
+        </Container>
+      </main>
+    </>
   )
 }
 
